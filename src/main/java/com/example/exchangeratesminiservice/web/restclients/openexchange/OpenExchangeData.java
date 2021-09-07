ac37fdb5115ebd5f06a6e8b41630d938320de2bc
eldar <eldarslavikovich@mail.ru>
2021-09-07 04:36:05 +0300

    Commented code was deleted

--- src/main/java/com/example/exchangeratesminiservice/web/restclients/openexchange/OpenExchangeData.java
+++ src/main/java/com/example/exchangeratesminiservice/web/restclients/openexchange/OpenExchangeData.java
@@ -1,15 +1,8 @@
 package com.example.exchangeratesminiservice.web.restclients.openexchange;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -18,15 +11,4 @@ public class OpenExchangeData {
     private String timestamp;
 
     private Map<String, String> rates;
-
-//    public void saveToJson() {
-//        List<String> ss = rates.keySet().stream().collect(Collectors.toList());
-//        ObjectMapper mapper = new ObjectMapper();
-//        Map<String, String> testMap = new HashMap<String, String>();
-//
-//        try {
-//            mapper.writeValue(new File("/Users/eldar/IdeaProjects/ExchangeRatesMiniService/currencies.json"), testMap);
-//        } catch (Exception s) {
-//        }
-//    }
 }
